03cabea3136c8ca53af7d34099ae0d6ccc722116
zii10 <lekent1992@gmail.com>
2026-04-15 22:25:33 +0800

    Add files via upload

---
+++ create3.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc = {
+  "name": "楊子青3",
+  "mail": "tcyang@pu.edu.tw",
+  "lab": 579
+}
+
+doc_ref = db.collection("靜宜資管2026a")
+doc_ref.add(doc)
---
+++ index.py
@@ -0,0 +1,102 @@
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
+
+from flask import Flask, render_template, request
+from datetime import datetime
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>歡迎來到周子洋Python網頁</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=子洋>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/account>網頁表單傳值</a><br>"
+    homepage += "<a href=/about>子洋簡介網頁</a><br>"
+    homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
+    homepage += "<a href=/search>老師查詢</a><br>"
+    return homepage
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    db = firestore.client()
+    results = []
+    keyword = ""
+    
+    if request.method == "POST":
+        keyword = request.form.get("keyword")
+        collection_ref = db.collection("靜宜資管2026a")
+        docs = collection_ref.get()
+
+        for doc in docs:
+            user = doc.to_dict()
+            if keyword in user["name"]:
+                results.append({
+                    "name": user["name"],
+                    "lab": user["lab"]
+                })
+
+    return render_template("search.html", results=results, keyword=keyword)
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+    
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    x=request.values.get("u")
+    y=request.values.get("dep")
+    return render_template("welcome.html", name=x,dep=y)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run()
