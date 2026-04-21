c783b0664cd15a7dd097fe30ca507b18c28038fc
zii10 <lekent1992@gmail.com>
2026-04-21 18:59:19 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -6,17 +6,27 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1.base_query import FieldFilter
 
-# 判斷是在 Vercel 還是本地
-if os.path.exists('serviceAccountKey.json'):
-    # 本地環境：讀取檔案
-    cred = credentials.Certificate('serviceAccountKey.json')
-else:
-    # 雲端環境：從環境變數讀取 JSON 字串
-    firebase_config = os.getenv('FIREBASE_CONFIG')
-    cred_dict = json.loads(firebase_config)
-    cred = credentials.Certificate(cred_dict)
-
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    if os.path.exists('serviceAccountKey.json'):
+        # 本地環境
+        cred = credentials.Certificate('serviceAccountKey.json')
+    else:
+        # 雲端環境 (Vercel)
+        firebase_config = os.getenv('FIREBASE_CONFIG')
+        if firebase_config:
+            try:
+                cred_dict = json.loads(firebase_config)
+                cred = credentials.Certificate(cred_dict)
+            except Exception as e:
+                print(f"JSON 解析錯誤: {e}")
+        else:
+            print("找不到環境變數 FIREBASE_CONFIG")
+    
+    # 執行初始化
+    try:
+        firebase_admin.initialize_app(cred)
+    except Exception as e:
+        print(f"Firebase 初始化失敗: {e}")
 
 
 from flask import Flask, render_template, request
